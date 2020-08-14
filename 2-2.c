2a9c45e5964ab9ee83eacb1b128d680f8cc6549c
yamato17 <yamato1033@gmail.com>
2020-08-14 12:57:54 +0000

    ed

--- 2-2.c
+++ 2-2.c
@@ -3,13 +3,21 @@
 
 int main(void){
     FILE *fp;
-    int c;
+    int count;
+    int ch;
     char data[MAXLEN];
-    if((fp=fopen("2-1result.txt","r")) == NULL){
-        printf("Cannot open file:2-1result.txt\n");
+    fp=fopen("2-2re.txt","r");
+    if(fp == NULL){
+        printf("Cannot open file:2-2re.txt\n");
         return -1;
     }
+    while(1){
+        ch=fgetc(fp);
+        if(ch==EOF) break;
+        count=count+1;
+    }
     fprintf(fp, data, stdout);
+    printf("%d",count);
 
     fclose(fp);
     return 0;
